57527b2108d89ab229dd8284067fc0e709c28413
hridayanp <hridayanphukan123@gmail.com>
2024-11-25 13:27:49 +0530

    Initial Commit

---
+++ ios-tip-calculatorTests/ios_tip_calculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  ios_tip_calculatorTests.swift
+//  ios-tip-calculatorTests
+//
+//  Created by Hridayan Phukan on 25/11/24.
+//
+
+import Testing
+@testable import ios_tip_calculator
+
+struct ios_tip_calculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
